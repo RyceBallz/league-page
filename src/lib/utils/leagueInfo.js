336dae4fc06c0bd8e7b6edd334824b57963317d1
RyceBallz <155477234+RyceBallz@users.noreply.github.com>
2026-09-03 14:39:17 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,7 +1,7 @@
 /*   STEP 1   */
 export const leagueID = "1030242699414253568"; // your league ID
 export const leagueName = "The Rice Bowl"; // your league name
-export const dues = 40; // (optional) used in template constitution page
+export const dues = 50; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
